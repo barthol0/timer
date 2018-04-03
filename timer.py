2cd45de425e75b44a580b0b3d5e8ddf2c521faae
bbialek <bialcus@gmail.com>
2018-04-04 00:59:41 +0100

    Added basic timer class.

--- timer.py
+++ timer.py
@@ -1,1 +1,25 @@
-#!/usr/bin/python3
+from datetime import datetime
+
+class Timer():
+    """A simple timer class"""
+    
+    def __init__(self):
+        pass
+    
+    def start_timer(self):
+        """Starts the timer"""
+        self.start = datetime.now()
+        return self.start
+    
+    def stop_timer(self):
+        """Stops the timer.  Returns the time elapsed"""
+        self.stop = datetime.now()
+        return str(self.stop - self.start)
+    
+    def now(self):
+        """Returns the current time with a message"""
+        return str(datetime.now())
+    
+    def elapsed(self):
+        """Time elapsed since start was called"""
+        return str(datetime.now() - self.start)
