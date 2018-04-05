5da0e6e7b42d67456ddfa380ceb095743e2d6912
bbialek <bialcus@gmail.com>
2018-04-05 11:51:10 +0100

    Added basic single line countdown timer.

--- timer.py
+++ timer.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from datetime import timedelta
+from time import sleep
 
 class Timer():
     """A simple timer class"""
@@ -15,15 +16,15 @@ class Timer():
     def stop_timer(self):
         """Stops the timer.  Returns the time elapsed"""
         self.stop = datetime.now()
-        return str(self.stop - self.start)
+        return self.stop - self.start
     
     def now(self):
-        """Returns the current time with a message"""
-        return str(datetime.now())
+        """Returns the current time"""
+        return datetime.now()
     
     def elapsed(self):
         """Time elapsed since start was called"""
-        return str(datetime.now() - self.start)
+        return datetime.now() - self.start
 
 
 # values taken from user input
@@ -31,4 +32,15 @@ pomodoros = 0
 pomodoro_time = 25
 break_time = 5
 
-eta = str(timedelta(seconds = 60 * pomodoro_time))
+eta = timedelta(seconds = 60 * pomodoro_time)
+target_time = datetime.now() + eta
+
+while True:
+    if target_time <= datetime.now():
+        print("Pomodoro!") 
+        break
+    else:
+        sleep(1)
+        eta = eta - timedelta(seconds=1)
+        print(str(eta), end='\r')
+
