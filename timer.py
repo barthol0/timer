f4f7777f3400728635c6e514158b7bb0374e0dd8
bbialek <bialcus@gmail.com>
2018-04-06 00:43:16 +0100

    Deleted dead code.

--- timer.py
+++ timer.py
@@ -2,31 +2,6 @@ from datetime import datetime
 from datetime import timedelta
 from time import sleep
 
-class Timer():
-    """A simple timer class"""
-    
-    def __init__(self):
-        pass
-    
-    def start_timer(self):
-        """Starts the timer"""
-        self.start = datetime.now()
-        return self.start
-    
-    def stop_timer(self):
-        """Stops the timer.  Returns the time elapsed"""
-        self.stop = datetime.now()
-        return self.stop - self.start
-    
-    def now(self):
-        """Returns the current time"""
-        return datetime.now()
-    
-    def elapsed(self):
-        """Time elapsed since start was called"""
-        return datetime.now() - self.start
-
-
 # values taken from user input
 pomodoros = 0
 pomodoro_time = 25
@@ -35,9 +10,15 @@ break_time = 5
 eta = timedelta(seconds = 60 * pomodoro_time)
 target_time = datetime.now() + eta
 
+print("##### POMODORO TIMER #####")
 while True:
     if target_time <= datetime.now():
         print("Pomodoro!") 
+        pomodoros += 1
+        if pomodoros == 4:
+            #long break
+            print('Take a longer break. You deserve it :)')
+            pomodoros = 0
         break
     else:
         sleep(1)
