496eda43696cfba63999d3c52d96b83848d75300
bbialek <bialcus@gmail.com>
2018-04-06 01:20:18 +0100

    Added short and long breaks.

--- timer.py
+++ timer.py
@@ -2,26 +2,36 @@ from datetime import datetime
 from datetime import timedelta
 from time import sleep
 
+def countdown(eta):
+    while eta > timedelta(seconds = 0):
+        sleep(1)
+        eta = eta - timedelta(seconds = 1)
+        print(str(eta), end='\r')
+
 # values taken from user input
 pomodoros = 0
 pomodoro_time = 25
-break_time = 5
+short_break_time = 5
+long_break_time = 15
 
-eta = timedelta(seconds = 60 * pomodoro_time)
-target_time = datetime.now() + eta
+pomodoro_eta = timedelta(seconds = 60 * pomodoro_time)
+short_break_eta = timedelta(seconds = 60 * short_break_time)
+long_break_eta = timedelta(seconds = 60 * long_break_time)
 
-print("##### POMODORO TIMER #####")
+target_time = datetime.now() + pomodoro_eta
+
+print('##### POMODORO TIMER #####')
 while True:
     if target_time <= datetime.now():
-        print("Pomodoro!") 
+        print('Pomodoro!') 
+        print('Take a short break!')
+        countdown(short_break_eta)
         pomodoros += 1
         if pomodoros == 4:
             #long break
             print('Take a longer break. You deserve it :)')
+            countdown(long_break_eta)
             pomodoros = 0
         break
     else:
-        sleep(1)
-        eta = eta - timedelta(seconds=1)
-        print(str(eta), end='\r')
-
+        countdown(pomodoro_eta)
