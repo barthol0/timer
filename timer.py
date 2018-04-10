40a0693c843065d45cc2e0e5534ca8e53613f2c4
bartholomeo <bialcus@gmail.com>
2018-04-10 17:19:25 +0100

    Deleted unnecessary if else statement.

--- timer.py
+++ timer.py
@@ -8,11 +8,14 @@ def countdown(eta):
         eta = eta - timedelta(seconds = 1)
         print(str(eta), end='\r')
 
-# values taken from user input
-pomodoros = 0
 pomodoro_time = 25
 short_break_time = 5
 long_break_time = 15
+
+#testing values
+# pomodoro_time = 0.1
+# short_break_time = 0.1
+# long_break_time = 0.1
 
 pomodoro_eta = timedelta(seconds = 60 * pomodoro_time)
 short_break_eta = timedelta(seconds = 60 * short_break_time)
@@ -25,15 +28,9 @@ while True:
     for i in range(4):
         print('Counting down...')
         countdown(pomodoro_eta)
-        print('Pomodoro!') 
-        pomodoros += 1
-        if pomodoros == 4:
-            #long break
-            print('Take a longer break. You deserve it :)')
-            countdown(long_break_eta)
-            pomodoros = 0
-            input('Press [ENTER] to continue.')
-        else:
-            print('Take a short break!')
-            countdown(short_break_eta) 
-            input('Press [ENTER] to continue')
+        print('Take a short break!')
+        countdown(short_break_eta) 
+        input('Press [ENTER] to continue')
+    print('Take a longer break. You deserve it :)')
+    countdown(long_break_eta)
+    input('Press [ENTER] to continue.')
